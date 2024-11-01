a32585a97aa76b866564880e7ec44c61e3f2871e
masa-maki <116690575+masa-maki@users.noreply.github.com>
2024-10-31 22:10:43 -0700

    rename file

--- app/test/ButtonTest.tsx
+++ app/test/ButtonTest.tsx
@@ -1,6 +1,6 @@
 // 'use client';
 import React from 'react'
-import { deleteUuidCookie, loginAction } from "./page";
+import { deleteUuidCookie, loginAction } from "./_page";
 
 export const ButtonTest = () => {
 	return (
